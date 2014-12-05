65052e61d19edbd0eb44a135e22e47991f73be31
Theo Pack <tf.pack@gmail.com>
2014-12-05 14:18:28 +0100

    Add test

--- test/woodpecker.test.js
+++ test/woodpecker.test.js
@@ -2,6 +2,11 @@ var woodpecker = require('../lib/woodpecker');
 var assert = require('assert');
 
 describe('woodpecker', function () {
+  afterEach(function(done) {
+    woodpecker.clean();
+    done();
+  });
+
   it('should return a registered service', function () {
     var myService = function () {
       return {
@@ -10,6 +15,28 @@ describe('woodpecker', function () {
     };
     woodpecker.service('myService', myService);
     var serviceFromWoodpecker = woodpecker.service('myService');
-    assert.equal(serviceFromWoodpecker.name, 'myService');
+    assert.strictEqual(serviceFromWoodpecker.name, 'myService');
+  });
+
+  it('should return a singleton service', function() {
+    var myService = function () {
+      return {
+        name: 'myService'
+      }
+    };
+    woodpecker.service('myService', myService);
+    var serviceFromWoodpecker = woodpecker.service('myService');
+    assert.strictEqual(woodpecker.service('myService'), serviceFromWoodpecker);
+  });
+
+  it('should return a prototype service', function() {
+    var myService = function () {
+      return {
+        name: 'myService'
+      }
+    };
+    woodpecker.service('myService', myService, false);
+    var serviceFromWoodpecker = woodpecker.service('myService');
+    assert.notStrictEqual(woodpecker.service('myService'), serviceFromWoodpecker);
   });
 });
