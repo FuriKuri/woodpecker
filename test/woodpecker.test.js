65ff0ac1635feda6bc354b01eff8812a33843e05
Theo Pack <tf.pack@gmail.com>
2014-12-05 11:47:53 +0100

    Add simple registry

--- test/woodpecker.test.js
+++ test/woodpecker.test.js
@@ -2,7 +2,14 @@ var woodpecker = require('../lib/woodpecker')
   , assert = require('assert');
 
 describe('woodpecker', function () {
-  it('should work as expected', function () {
-    assert.equal(2, woodpecker.sum(1, 1));
-  })
+  it('should return a registered service', function () {
+    var myService = function () {
+      return {
+        name: 'myService'
+      }
+    };
+    woodpecker.service('myService', myService);
+    var serviceFromWoodpecker = woodpecker.service('myService');
+    assert.equal(serviceFromWoodpecker.name, 'myService');
+  });
 });
