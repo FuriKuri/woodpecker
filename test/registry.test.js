073538d2ed932aa484b09c9ab481cc9936fea46d
Theo Pack <tf.pack@gmail.com>
2014-12-07 09:40:13 +0100

    Change data structor

--- test/registry.test.js
+++ test/registry.test.js
@@ -19,12 +19,14 @@ describe('registry', function () {
 
   it('should return a singleton service function', function() {
     registry.save('service', myService, true);
-    assert.equal(registry.load('service'), myService);
+    assert.equal(registry.load('service').constructor, myService);
+    assert.equal(registry.load('service').singleton, true);
   });
 
   it('should return a prototype service function', function() {
     registry.save('service', myService, false);
-    assert.equal(registry.load('service'), myService);
+    assert.equal(registry.load('service').constructor, myService);
+    assert.equal(registry.load('service').singleton, false);
   });
 
   it('should return null if no service was registered', function() {
