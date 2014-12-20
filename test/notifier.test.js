c9684a91b45b6a4f306f0b4b4ce25b154aeabaca
Theo Pack <tf.pack@gmail.com>
2014-12-20 19:06:35 +0100

    Add tests

---
+++ test/notifier.test.js
@@ -0,0 +1,60 @@
+var notifier = require('../lib/notifier');
+var assert = require('assert');
+
+describe('notifier', function () {
+  function load() {
+    return {
+      name: 'new'
+    };
+  }
+
+  it('should call the callback function', function() {
+    var services = {};
+    services.service = {
+      name: 'service',
+      constructor: function () {},
+      singleton: true,
+      dependencies: [],
+      instances: [],
+      callback: null
+    };
+
+    services.myService = {
+      name: 'myService',
+      constructor: function () {},
+      singleton: true,
+      dependencies: [ 'service' ],
+      instances: [ {} ],
+      callback: function (obj, name, instance) {
+        assert.equal(obj.toString(), {});
+        assert.equal(name, 'service');
+        assert.equal(instance.name, 'new');
+      }
+    };
+
+    notifier(services, 'service', load);
+  });
+
+  it('should call nothing if no callback exists', function() {
+    var services = {};
+    services.service = {
+      name: 'service',
+      constructor: function () {},
+      singleton: true,
+      dependencies: [],
+      instances: [],
+      callback: null
+    };
+
+    services.myService = {
+      name: 'myService',
+      constructor: function () {},
+      singleton: true,
+      dependencies: [ 'service' ],
+      instances: [ {} ],
+      callback: null
+    };
+
+    notifier(services, 'service', load);
+  });
+});
