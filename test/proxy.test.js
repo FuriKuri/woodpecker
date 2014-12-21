4e8563e2a037cc649bcc23f99db428e9dd550282
Theo Pack <tf.pack@gmail.com>
2014-12-21 22:56:58 +0100

    Add proxy class

---
+++ test/proxy.test.js
@@ -0,0 +1,30 @@
+var ServiceProxy = require('../lib/proxy');
+var assert = require('assert');
+
+describe('ServiceProxy', function () {
+
+  it('should create a proxy instance', function() {
+    assert.notEqual(new ServiceProxy(), null);
+  });
+
+  it('should create a proxy instance even if new is missing', function() {
+    assert.notEqual(require('../lib/proxy')(), null);
+  });
+
+  it('should create delegate methods', function() {
+    var proxy = new ServiceProxy();
+    var service = {};
+    service.name = function() { return 'Name' };
+    proxy.service = service;
+    assert.equal(proxy.name(), 'Name');
+  });
+
+  it('should remove old methods when new service is set', function() {
+    var proxy = new ServiceProxy();
+    var service = {};
+    service.name = function() { return 'Name' };
+    proxy.service = service;
+    proxy.service = {};
+    assert.equal(proxy.name, undefined);
+  });
+});
